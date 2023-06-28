ef6d3161fd164f908b6abee718ffd7db5fa42f35
r-heart <rachaelkheart@gmail.com>
2023-06-28 10:56:06 -0700

    feat: :sparkles: write App.jsx with imported components and hooks
    write handleChange function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import Todo from "./components/Todo";
+import useFetch from "./hooks/useFetch";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [todos, setTodos] = useFetch(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
+  const handleChange = (event) => {
+    setTodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id.toString() === Number(event.target.id)) {
+          return { ...todo, completed: event.target.checked };
+        }
+        return todo;
+      });
+    });
+  };
+
+  const numOfCompletedTodos = todos.filter((todo) => todo.completed).length;
+
+  return (
+    <>
+      <h1 className="text-center text-3xl font-bold">
+        {numOfCompletedTodos} of {todos.length} To Dos Completed
+      </h1>
+
+      <ul className="flex flex-col">
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} onChange={handleChange} />
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
