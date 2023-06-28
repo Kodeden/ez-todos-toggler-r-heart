2bc3a3f6e0f9a2a248fc60fe2b8265251887d4f9
r-heart <rachaelkheart@gmail.com>
2023-06-28 12:44:49 -0700

    test: :white_check_mark: update original test, write second test, and pass tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,15 +10,20 @@ it("renders the number of completed todos (90 at initial load) in the heading", 
   await screen.findAllByRole("checkbox");
 
   const heading = screen.getByRole("heading");
-  expect(heading).toHaveTextContent("90 / 200 Completed");
+  expect(heading).toHaveTextContent("90 of 200 To Dos Completed");
 });
 
-it("renders the number of completed todos (91 after one todo is checked) in the heading", () => {
+it("renders the number of completed todos (91 after one todo is checked) in the heading", async () => {
   const user = userEvent.setup();
   render(<App />);
 
   // TODO: Write a test that checks that the number of completed todos is 91 after one todo is checked
   // Hint: use userEvent.click() to check a todo
+  const checkboxes = await screen.findAllByRole("checkbox");
+  await user.click(checkboxes[0]);
+
   // Hint: use screen.getByRole("heading") to get the heading
+  const heading = screen.getByRole("heading");
   // Hint: use expect().toHaveTextContent() to check the heading text
+  expect(heading).toHaveTextContent("91 of 200 To Dos Completed");
 });
