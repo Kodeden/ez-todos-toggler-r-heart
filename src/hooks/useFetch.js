0b9c01730b5889a172a2b00be671a74906db920e
r-heart <rachaelkheart@gmail.com>
2023-06-28 10:17:36 -0700

    feat: :sparkles: write useFetch custom hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,2 +1,15 @@
+import React from "react";
 
+export default function useFetch(url) {
+  const [data, setData] = React.useState([]);
 
+  React.useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [url]);
+
+  return [data, setData];
+}
